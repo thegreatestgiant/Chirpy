6c70f6d6949a27fd394225122ab3157c0c46ac39
Sean Alter <seanalter1@gmail.com>
2023-09-06 23:52:29 -0400

    Add handlerReadiness

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -11,14 +10,22 @@ func main() {
 	const fileServerPath = "."
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(fileServerPath)))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(fileServerPath))))
+	mux.HandleFunc("/healthz", handlerReadiness)
+
 	corsMux := middlewareCors(mux)
 
-	server := http.Server{
+	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: corsMux,
 	}
 
-	fmt.Printf("Listening to FileServer in %s on port %v\n", fileServerPath, port)
+	log.Printf("Listening to FileServer in %s on port %v\n", fileServerPath, port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	r.Header.Add("Content-Type", "text/plain; charset=utf-8 ")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
 }
