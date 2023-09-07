24a5cc9cc72d17aa1ba33f5e6d0d0f1e3ae98d6c
Sean Alter <seanalter1@gmail.com>
2023-09-06 23:03:55 -0400

    Add fileserver

--- main.go
+++ main.go
@@ -8,14 +8,17 @@ import (
 
 func main() {
 	const port = "8000"
+	const fileServerPath = "."
 
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(fileServerPath)))
 	corsMux := middlewareCors(mux)
 
 	server := http.Server{
 		Addr:    ":" + port,
 		Handler: corsMux,
 	}
+
 	fmt.Printf("Listening on port %v\n", port)
 	log.Fatal(server.ListenAndServe())
 }
