42e737f93763d1859d1b83fb38ec51c753cea60a
Sean Alter <seanalter1@gmail.com>
2023-09-07 23:19:54 -0400

    Update paths and spread out code among files

---
+++ internal/database/chirps.go
@@ -0,0 +1,45 @@
+package database
+
+import "os"
+
+func (db *DB) CreateChirp(body string) (Chirp, error) {
+	dbStructure, err := db.loadDB()
+	if err != nil {
+		return Chirp{}, err
+	}
+	id := len(dbStructure.Chirps) + 1
+	NewChirp := Chirp{
+		ID:   id,
+		Body: body,
+	}
+	dbStructure.Chirps[NewChirp.ID] = NewChirp
+	err = db.writeDB(dbStructure)
+	if err != nil {
+		return Chirp{}, err
+	}
+	return NewChirp, nil
+}
+
+func (db *DB) GetChirps() ([]Chirp, error) {
+	dbStructure, err := db.loadDB()
+	if err != nil {
+		return nil, err
+	}
+	chirps := make([]Chirp, 0, len(dbStructure.Chirps))
+	for _, chirp := range dbStructure.Chirps {
+		chirps = append(chirps, chirp)
+	}
+	return chirps, nil
+}
+
+func (db *DB) GetChirp(id int) (Chirp, error) {
+	dbStructure, err := db.loadDB()
+	if err != nil {
+		return Chirp{}, err
+	}
+	chirps, ok := dbStructure.Chirps[id]
+	if !ok {
+		return Chirp{}, os.ErrNotExist
+	}
+	return chirps, nil
+}
--- internal/database/database.go
+++ internal/database/database.go
@@ -22,8 +22,6 @@ type Chirp struct {
 	ID   int    `json:"id"`
 }
 
-
-
 func NewDB(path string) (*DB, error) {
 	db := &DB{
 		Path: path,
@@ -31,48 +29,6 @@ func NewDB(path string) (*DB, error) {
 	}
 	err := db.ensureDB()
 	return db, err
-}
-
-func (db *DB) CreateChirp(body string) (Chirp, error) {
-	dbStructure, err := db.loadDB()
-	if err != nil {
-		return Chirp{}, err
-	}
-	id := len(dbStructure.Chirps) + 1
-	NewChirp := Chirp{
-		ID:   id,
-		Body: body,
-	}
-	dbStructure.Chirps[NewChirp.ID] = NewChirp
-	err = db.writeDB(dbStructure)
-	if err != nil {
-		return Chirp{}, err
-	}
-	return NewChirp, nil
-}
-
-func (db *DB) GetChirps() ([]Chirp, error) {
-	dbStructure, err := db.loadDB()
-	if err != nil {
-		return nil, err
-	}
-	chirps := make([]Chirp, 0, len(dbStructure.Chirps))
-	for _, chirp := range dbStructure.Chirps {
-		chirps = append(chirps, chirp)
-	}
-	return chirps, nil
-}
-
-func (db *DB) GetChirp(id int) (Chirp, error) {
-	dbStructure, err := db.loadDB()
-	if err != nil {
-		return Chirp{}, err
-	}
-	chirps, ok := dbStructure.Chirps[id]
-	if !ok {
-		return Chirp{}, os.ErrNotExist
-	}
-	return chirps, nil
 }
 
 func (db *DB) createDB() error {
